d7810993dc221351ea453b568d502e17bf9343f8
matomugo60 <matomugo60@gmail.com>
2023-03-27 14:09:09 +0300

    Updated login.js to use backend

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -2,62 +2,68 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Login() {
-  const [usernameOrEmail, setUsernameOrEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+const [usernameOrEmail, setUsernameOrEmail] = useState('');
+const [password, setPassword] = useState('');
+const [errorMessage, setErrorMessage] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      // Login logic done  here. 
+const handleLogin = async () => {
+try {
+const response = await fetch('/api/login', {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ usernameOrEmail, password })
+});
+if (response.ok) {
+  window.location.href = '/todo';
+} else {
+  const error = await response.json();
+  setErrorMessage(error.message);
+}
+} catch (error) {
+setErrorMessage(error.message);
+}
+};
 
-      // Redirect to /todo on successful login
-      window.location.href = '/todo';
-
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
-  };
-
-  return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-4">
-          <h1>Login</h1>
-          <div className="mb-3">
-            <label htmlFor="usernameOrEmail" className="form-label">
-              Username or Email
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="usernameOrEmail"
-              value={usernameOrEmail}
-              onChange={(e) => setUsernameOrEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-          <Link to="/todo" className="btn btn-primary" onClick={handleLogin}>
-            Login
-          </Link>
-          <div className="mt-3">
-            Don't have an account? <Link to="/">Sign up</Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+return (
+<div className="container mt-5">
+<div className="row justify-content-center">
+<div className="col-md-4">
+<h1>Login</h1>
+<div className="mb-3">
+<label htmlFor="usernameOrEmail" className="form-label">
+Username or Email
+</label>
+<input
+type="text"
+className="form-control"
+id="usernameOrEmail"
+value={usernameOrEmail}
+onChange={(e) => setUsernameOrEmail(e.target.value)}
+/>
+</div>
+<div className="mb-3">
+<label htmlFor="password" className="form-label">
+Password
+</label>
+<input
+type="password"
+className="form-control"
+id="password"
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+/>
+</div>
+{errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+<button className="btn btn-primary" onClick={handleLogin}>
+Login
+</button>
+<div className="mt-3">
+Don't have an account? <Link to="/">Sign up</Link>
+</div>
+</div>
+</div>
+</div>
+);
 }
 
 export default Login;
