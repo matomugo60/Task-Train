caa79a086aee7d2cf7485eed61ced0380d31db3c
matomugo60 <matomugo60@gmail.com>
2023-03-27 17:12:24 +0300

    updated PUT method in Todo.js

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -2,68 +2,73 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [editingTodo, setEditingTodo] = useState(null);
-  const [editTitle, setEditTitle] = useState('');
-  const [editDescription, setEditDescription] = useState('');
-  const [showPopup, setShowPopup] = useState(false);
-// Get todos
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const response = await axios.get('https://td-api-3e6u.onrender.com/todos');
-        setTodos(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchTodos();
-  }, []);
-// Add todo
-  const handleAddTodo = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://td-api-3e6u.onrender.com/todos', { title, description });
-      setTodos([...todos, response.data]);
-      setTitle('');
-      setDescription('');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-// Update todo
-  const handleUpdateTodo = async (id) => {
-    try {
-      const updatedTodo = { title: editTitle, description: editDescription };
-      const response = await axios.patch(`https://td-api-3e6u.onrender.com/todos/${id}`, updatedTodo);
-      const updatedTodos = todos.map((todo) => (todo.id === response.data.id ? response.data : todo));
-      setTodos(updatedTodos);
-      setEditingTodo(null);
-      setShowPopup(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-// Delete todo
-  const handleDeleteTodo = async (id) => {
-    try {
-      await axios.delete(`https://td-api-3e6u.onrender.com//todos/${id}`);
-      const filteredTodos = todos.filter((todo) => todo.id !== id);
-      setTodos(filteredTodos);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-// Edit todo using a popup
-  const handleEditTodo = (todo) => {
-    setEditingTodo(todo);
-    setEditTitle(todo.title);
-    setEditDescription(todo.description);
-    setShowPopup(true);
-  };
+const [todos, setTodos] = useState([]);
+const [title, setTitle] = useState('');
+const [description, setDescription] = useState('');
+const [editingTodo, setEditingTodo] = useState(null);
+const [editTitle, setEditTitle] = useState('');
+const [editDescription, setEditDescription] = useState('');
+const [showPopup, setShowPopup] = useState(false);
+const [error, setError] = useState('');
 
+// Get todos
+useEffect(() => {
+const fetchTodos = async () => {
+try {
+const response = await axios.get('https://td-api-3e6u.onrender.com/todos');
+setTodos(response.data);
+} catch (error) {
+setError('Error fetching todos');
+}
+};
+fetchTodos();
+}, []);
+
+// Add todo
+const handleAddTodo = async (e) => {
+e.preventDefault();
+try {
+const response = await axios.post('https://td-api-3e6u.onrender.com/todos', { title, description });
+setTodos([...todos, response.data]);
+setTitle('');
+setDescription('');
+} catch (error) {
+setError('Error adding todo');
+}
+};
+
+// Update todo
+const handleUpdateTodo = async (id) => {
+try {
+const updatedTodo = { title: editTitle, description: editDescription };
+const response = await axios.put(`https://td-api-3e6u.onrender.com/todos/${id}`, updatedTodo);
+const updatedTodos = todos.map((todo) => (todo.id === response.data.id ? response.data : todo));
+setTodos(updatedTodos);
+setEditingTodo(null);
+setShowPopup(false);
+} catch (error) {
+setError('Error updating todo');
+}
+};
+
+// Delete todo
+const handleDeleteTodo = async (id) => {
+try {
+await axios.delete(`https://td-api-3e6u.onrender.com/todos/${id}`);
+const filteredTodos = todos.filter((todo) => todo.id !== id);
+setTodos(filteredTodos);
+} catch (error) {
+setError('Error deleting todo');
+}
+};
+
+// Edit todo using a popup
+const handleEditTodo = (todo) => {
+setEditingTodo(todo);
+setEditTitle(todo.title);
+setEditDescription(todo.description);
+setShowPopup(true);
+};
   return (
     <div className="container">
       <h1>Todo App</h1>
